95123bde117c0661ca66eb0eaec7dbe0ccdafe79
luhatfimhalaf <luhatfim.halaf1@gmail.com>
2025-06-07 16:13:22 +0700

    delete all

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RouteController;
-
-Route::middleware('api')->group(function () {
-    Route::apiResource('routes', RouteController::class);
-});
